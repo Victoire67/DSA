d10fc98b95cdb442b2da83f7abc557122de7fa97
Victoire Ansima <ansimavicky@gmail.com>
2026-05-10 17:19:01 +0200

    ft : added sign algebra

--- Arrays/binomialExpansion.ts
+++
@@ -1,8 +0,0 @@
-function expand(expression: string): string {
-    
-    return expression;
-}
-let expression = "(12x + b)^5"
-console.log(expand(expression))
-
-//expand("(x+1)^2");      // returns "x^2+2x+1"
